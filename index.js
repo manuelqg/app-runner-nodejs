0ccf67d59aaaa4cd15fa4cb8ef6ddf172e3b2d19
Manuel Quijano Gaviria <manuelqg@amazon.com>
2024-10-02 15:39:28 -0600

    First Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// Create express basic server
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(8080, () => {
+  console.log('Server is running on port 8080');
+});
